48d3b5eb6fcd9855f0366ded18ff579f9bcee88a
sachin senal <127573781+sachinsenal0x64@users.noreply.github.com>
2024-02-10 21:53:57 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   docus: {
-    title: 'Hifi Tui',
+    title: 'Hifi-Tui',
     description: 'Cross Platform Premium Tidal Hifi Music Player In Your Terminal.',
     image: 'https://sachinsenal0x64.github.io/picx-images-hosting/cover.5gnodmhum874.webp',
     socials: {
