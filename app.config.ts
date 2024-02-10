2cd97d93280e874291cb5b28d0e2119e39915707
sachin senal <127573781+sachinsenal0x64@users.noreply.github.com>
2024-02-10 15:59:26 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -5,6 +5,11 @@ export default defineAppConfig({
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       github: 'sachinsenal0x64/Hifi-Tui',
+      discord: {
+	      icon: 'fa-brands:discord',
+	      label: 'discord',
+	      href: 'https://discord.gg/EbfftZ5Dd4'
+      }
     },
     aside: {
       level: 0,
